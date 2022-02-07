47f02bde151b65f4c58c3020b7393f5faf9012ee
Lívia Siqueira <livia.siqueira12@outlook.com>
2022-02-07 14:26:54 -0300

    feat: iniciando com react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useForm } from "react-hook-form";
+import LogRocket from 'logrocket';
+LogRocket.init('fzxpr4/learning-react-hook-form');
+
+interface valuesForm {
+  Name: string;
+  Email: string;
+  Telephone: string;
+}
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      Name: "",
+      Email: "",
+      Telephone: "",
+    },
+  });
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [telephone, setTelephone] = useState("");
+
+  const onFormSubmit = (data: valuesForm) => console.log(data);
+  const problemFormSubmit = (error: any) => console.log(error);
+
+  const submitFormControlled = () => {
+    console.log(email, telephone, name);
+  };
+
+  const registerValidation = {
+    name: { required: "Name is required" },
+    email: { required: "Email is required" },
+    telephone: { required: "Telephone is required" },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>React-Hook-Form</h2>
+        <form onSubmit={handleSubmit(onFormSubmit, problemFormSubmit)}>
+          <input
+            placeholder="Name"
+            {...register("Name", registerValidation.name)}
+          />
+          {errors?.Name && errors.Name?.message}
+          <input
+            placeholder="Email"
+            {...register("Email", registerValidation.email)}
+          />
+          {errors?.Email && errors.Email?.message}
+          <input
+            placeholder="Telephone"
+            {...register("Telephone", registerValidation.telephone)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div>
+        <h2>Components Controllers</h2>
+        <form onSubmit={submitFormControlled}>
+          <input placeholder="Name" onChange={(e) => setName(e.target.value)} />
+          <input
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Telephone"
+            onChange={(e) => setTelephone(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
